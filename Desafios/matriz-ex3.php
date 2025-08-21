3a4aab8d3e872c89f5df8d22ca591f453e4f5107
Gabriel Philippe <philippegabriel680@gmail.com>
2025-08-21 18:17:07 -0300

    :sparkles: feat: Adicionando atividades de matrizes

--- Desafios/matriz-ex3.php
+++ Desafios/matriz-ex3.php
@@ -1,8 +1,8 @@
 <?php
 $matriz = [
- [1, 2, 3],
- [4, 5, 6],
- [7, 8, 9]
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
 ];
 
 foreach ($matriz as $linha) {
@@ -11,3 +11,14 @@ foreach ($matriz as $linha) {
     }
     echo PHP_EOL;
 }
+
+$todos_numeros = array_merge(...$matriz);
+$numeros_unicos = array_unique($todos_numeros);
+
+if (count($todos_numeros) === 9 && count($numeros_unicos) === 9 && max($todos_numeros) <= 9 &&  min($todos_numeros) >= 1) {
+    echo "\nSudoku válido";
+} else {
+    echo "\nSudoku inválido";
+}
+
+?>
---
+++ Desafios/matriz-parte2-ex4.php
@@ -0,0 +1,12 @@
+<?php
+$insiraX = readline('Insira o número que está atrás de buscar > ');
+
+$numerosNX = [10, 20, 30, 40, 50];
+
+if (in_array($insiraX, $numerosNX)) {
+    $posicao = array_search($insiraX, $numerosNX);
+    echo 'Número encontrado e está na posição: ' . $posicao;
+} else {
+    echo 'Número não encontrado!';
+}
+?>
